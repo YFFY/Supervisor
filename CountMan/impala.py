1fa359bfd35832fd28287ffeac0a9d5bb4676552
yfsuse <yfsuse@gmail.com>
2015-04-07 10:17:04 +0800

    update impala data used int() method if this data is string

--- CountMan/impala.py
+++ CountMan/impala.py
@@ -12,13 +12,13 @@ def getImpalaData():
     try:
         clickCommand = """impala-shell -i {0} -q 'use ym_system;select count(1) from ym_impala where part1="{1}" and log_tye =0;'""".format(IMPALA_IP, day)
         convCommand = """impala-shell -i {0} -q 'use ym_system;select count(1) from ym_impala where part1="{1}" and log_tye =1 and datasource!="hasoffer" and status="Confirmed";'""".format(IMPALA_IP, day)
-        impalaData['impalaClick'] = os.popen(clickCommand).read().split()[-3]
-        impalaData['impalaConv'] = os.popen(convCommand).read().split()[-3]
+        impalaData['impalaClick'] = int(os.popen(clickCommand).read().split()[-3])
+        impalaData['impalaConv'] = int(os.popen(convCommand).read().split()[-3])
         return impalaData
     except Exception as ex:
         # traceback.print_exc()
         pass
-    return {"impalaClick": 0, "impalaConv": 0}
+    return {"impalaClick": -1, "impalaConv": -1}
 
 def set2db():
     dao = DatabaseInterface()
